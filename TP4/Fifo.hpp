0572e2438f92edaf5460b72fab71e5135009a48a
iad-ABDUL-RAOUF <iad_abdulraouf@yahoo.fr>
2021-04-04 20:12:20 +0200

    Fifo devellope et compile

---
+++ TP4/Fifo.hpp
@@ -0,0 +1,86 @@
+#ifndef Fifo_hpp_INCLUDED
+#define Fifo_hpp_INCLUDED
+
+#include <queue>
+#include "Mutex.hpp"
+
+
+// header
+template <class T>
+class Fifo
+{
+public:
+  class EmptyException;
+
+public:
+  Fifo();
+  ~Fifo();
+  void push(T element);
+  T pop();
+  T pop (double timeout_ms);
+
+private:
+  std::queue<T> elements;
+  Mutex mutex;
+};
+
+
+
+// implementation
+template <class T>
+Fifo<T>::Fifo()
+{
+  mutex = Mutex();
+}
+
+template <class T>
+ Fifo<T>::~Fifo()
+{
+}
+
+template <class T>
+void Fifo<T>::push(T element)
+{
+  {
+    Mutex::Lock lock(mutex);
+    elements.push(element);
+    lock.notify();
+  }
+}
+
+template <class T>
+T Fifo<T>::pop()
+{
+  T element;
+  {
+    Mutex::Lock lock(mutex);
+    while (elements.empty)
+    {
+      lock.wait();
+    }
+    element = elements.front();
+    elements.pop();
+  }
+  return element;
+}
+
+template <class T>
+T Fifo<T>::pop(double timeout_ms)
+{
+  T element;
+  {
+    Mutex::Lock lock(mutex);
+    while (elements.empty)
+    {
+      if (lock.wait(timeout_ms))
+      {
+        throw EmptyException("timed out in Fifo pop");
+      }
+    }
+    element = elements.front();
+    elements.pop();
+  }
+  return element;
+}
+
+#endif
