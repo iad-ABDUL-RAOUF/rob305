76dfe06569d6f0a7a1c4ed82f4d4b7368734911a
MAlphaBecker <madeleine.becker@enta-paristech.fr>
2021-04-04 21:51:34 +0200

    TP4c

--- TP4/td4c.cpp
+++ TP4/td4c.cpp
@@ -5,22 +5,36 @@
 #include <string>
 #include <vector>
 
-
 // TokenProducer produce token for semaphore
 class TokenProducer : public Thread
+/**
+ * \class TokenProducer
+ * \brief gives a token to the semaphore
+ */
 {
 public:
-  TokenProducer(Semaphore& semaphore,double delay_ms = 0);
+  TokenProducer(Semaphore &semaphore, double delay_ms = 0);
+  /**
+   * \brief constructor of an instance of the TokenProducer
+   * \param semaphore Semaphore instance to give token to
+   * \param delay_ms delay before a token is given
+   */
   ~TokenProducer();
+  /**
+   * \brief destructor of the TokenProducer
+   */
   void run() override;
+  /**
+   * \brief gives a token to the semaphore
+   */
 
 private:
-  Semaphore& semaphore;
-  double delay_ms;
+  Semaphore &semaphore; /** Semaphore instance to give token to */
+  double delay_ms;      /** delay before a token is given */
 };
 
-TokenProducer::TokenProducer(Semaphore& semaphore, double delay_ms) 
-: Thread(), semaphore(semaphore), delay_ms(delay_ms)
+TokenProducer::TokenProducer(Semaphore &semaphore, double delay_ms)
+    : Thread(), semaphore(semaphore), delay_ms(delay_ms)
 {
 }
 
@@ -35,21 +49,34 @@ void TokenProducer::run()
   semaphore.give();
 }
 
-
 // TokenConsumer consume token from semaphore
 class TokenConsumer : public Thread
+/**
+ * \class TokenProducer
+ * \brief takes a token from the semaphore
+ */
 {
 public:
-  TokenConsumer(Semaphore& semaphore);
+  TokenConsumer(Semaphore &semaphore);
+  /**
+   * \brief constructor of an instance of the TokenConsumer
+   * \param semaphore Semaphore instance to take token from
+   */
   ~TokenConsumer();
+  /**
+   * \brief destructor of the TokenConsumer class
+   */
   void run() override;
+  /**
+   * \brief takes a token from the semaphore
+   */
 
 private:
-  Semaphore& semaphore;
+  Semaphore &semaphore; /** Semaphore instance to take token from */
 };
 
-TokenConsumer::TokenConsumer(Semaphore& semaphore) 
-: Thread(), semaphore(semaphore)
+TokenConsumer::TokenConsumer(Semaphore &semaphore)
+    : Thread(), semaphore(semaphore)
 {
 }
 
@@ -62,17 +89,17 @@ void TokenConsumer::run()
   semaphore.take();
 }
 
-
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
   //read parameters
   if (argc != 3)
   {
-    std::cerr << "USAGE: " << "<nCons> <nProd>" << std::endl;
+    std::cerr << "USAGE: "
+              << "<nCons> <nProd>" << std::endl;
     return 1;
   }
   std::string params;
-  for (int i=1; i < argc; ++i)
+  for (int i = 1; i < argc; ++i)
   {
     params += argv[i];
     params += " ";
@@ -82,29 +109,40 @@ int main(int argc, char* argv[])
   unsigned int nProd;
   is >> nCons;
   is >> nProd;
+
   // init variables
   Semaphore semaphore;
   std::vector<TokenProducer> tokenProducer;
   std::vector<TokenConsumer> tokenConsumer;
-  for (unsigned int i = 0; i < nCons; i++) {
+  for (unsigned int i = 0; i < nCons; i++)
+  {
     tokenConsumer.push_back(TokenConsumer(semaphore));
   }
-  for (unsigned int i = 0; i < nProd; i++) {
+  for (unsigned int i = 0; i < nProd; i++)
+  {
     tokenProducer.push_back(TokenProducer(semaphore));
   }
+
   // start threads
-  for (unsigned int i = 0; i < nCons; i++) {
+  std::cout << "starting threads " << std::endl;
+  for (unsigned int i = 0; i < nCons; i++)
+  {
     tokenConsumer[i].start();
   }
-  for (unsigned int i = 0; i < nProd; i++) {
+  for (unsigned int i = 0; i < nProd; i++)
+  {
     tokenProducer[i].start();
   }
+
   // wait for end of threads
-  for (unsigned int i = 0; i < nCons; i++) {
+  for (unsigned int i = 0; i < nCons; i++)
+  {
     tokenConsumer[i].join();
   }
-  for (unsigned int i = 0; i < nProd; i++) {
+  for (unsigned int i = 0; i < nProd; i++)
+  {
     tokenProducer[i].join();
   }
+  std::cout << "threads ended" << std::endl;
   return 0;
 }
