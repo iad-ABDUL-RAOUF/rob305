7f1fce5b86452b3339124ad08ae835efd0d5ceca
MAlphaBecker <madeleine.becker@enta-paristech.fr>
2021-04-04 18:25:14 +0200

    TP3b

--- TP3/CountDown.hpp
+++ TP3/CountDown.hpp
@@ -1,14 +1,28 @@
 #include "Timer.hpp"
 
 class CountDown : public PeriodicTimer
+/**
+ * \class CountDown
+ * \brief implements a periodic timer that decrements a counter
+ */
 {
-  public:
-    CountDown(unsigned int initCount);
-    unsigned int count() const {return count_;}
+public:
+  CountDown(unsigned int initCount);
+  /**
+     * \brief constructor of an instance of the CountDown class
+     * \param initCount counter value at the beginning of the countdown
+     */
+  unsigned int count() const { return count_; }
+  /**
+     * \brief checks the value of the counter
+     */
 
-  private :
-    void callback() override;
-  
-  private :
-    unsigned int count_;
+private:
+  void callback() override;
+  /**
+     * \brief callback function when the timer ends, decrements the counter
+     */
+
+private:
+  unsigned int count_; /** counter to be decremented */
 };
